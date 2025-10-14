48a61ce13ac671d84d92cffe48478796fbb8030a
Marcus <marmar11137@gmail.com>
2025-10-14 13:57:05 -0400

    Certicate and counter added

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
+var click = 0
+function fun(){
+   document.getElementById("counts").innerHTML = ++click
+    click = click
+}
+
+
+
 openModalButtons.forEach(button => {
     button.addEventListener("click", () => {
         const modal = document.querySelector(button.dataset.modalTarget)
